2e9dcdd0e1b95536061fc6818f6bffd324d1a71a
ANDREA CAMILA SOTOMAYOR PINO <017201510I@uandina.edu.pe>
2021-04-12 09:36:51 -0500

    Final

---
+++ slnUniversida/Cliente CSharp/Login.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Cliente_CSharp.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Login ID="Login1" runat="server" BackColor="#F7F7DE" BorderColor="#CCCC99" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="10pt" OnAuthenticate="Login1_Authenticate">
+    <TitleTextStyle BackColor="#6B696B" Font-Bold="True" ForeColor="#FFFFFF" />
+</asp:Login>
+</asp:Content>
---
+++ slnUniversida/Cliente CSharp/Login.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cliente_CSharp
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        ServiceReferenceUsuario.wsLoginSoapClient servicio = new ServiceReferenceUsuario.wsLoginSoapClient();
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            string usuario = Login1.UserName;
+            string password = Login1.Password;
+            string[] valores = servicio.Login(usuario, password).ToArray();
+            if (valores[0] == "0" && valores[1] == "Alumno")
+            {
+                FormsAuthentication.RedirectFromLoginPage(usuario, false);
+            }
+            else if (valores[0] == "0" && valores[1] == "Docente")
+            {
+                Login1.FailureText = "Usted es docente, no tiene acceso al sistema";
+            }
+            else
+                Login1.FailureText = valores[1];
+        }
+    }
+}
